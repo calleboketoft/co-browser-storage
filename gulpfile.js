093871164292d909908d2ac074c5489e13080be2
Carl-Ola Boketoft <calleboketoft@gmail.com>
2016-01-20 12:47:03 +0100

    use gulp for typescript watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
+// SASS
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 gulp.task('sass', function () {
   gulp.src('./node_modules/bootstrap/scss/bootstrap.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./src/css'))
+})
+
+// TypeScript
+var ts = require('gulp-typescript')
+var sourcemaps = require('gulp-sourcemaps')
+var tsProject = ts.createProject('src/tsconfig.json')
+gulp.task('typescript', function () {
+  var tsResult = gulp.src('./src/**/*.ts')
+    .pipe(sourcemaps.init())
+    .pipe(ts(tsProject))
+
+  return tsResult.js
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./src'))
+})
+gulp.task('typescript:watch', ['typescript'], function () {
+  gulp.watch('./src/**/*.ts', ['typescript'])
 })
---
+++ src/systemjs.config.ts
@@ -0,0 +1,12 @@
+declare var System
+
+System.config({
+  baseURL: '/',
+  defaultJSExtensions: true,
+  warnings: true,
+  map: {
+    'angular2': 'node_modules/angular2',
+    'rxjs': 'node_modules/rxjs',
+    '@ngrx': 'node_modules/@ngrx/store/dist'
+  }
+})
