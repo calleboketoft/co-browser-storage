2a2abb797acdd547aaa3d3d22fc6067f2609de06
Carl-Ola Boketoft <calleboketoft@gmail.com>
2016-03-07 22:07:20 +0100

    smarter exports

---
+++ co-browser-storage.js
@@ -0,0 +1,8 @@
+"use strict";
+var co_browser_storage_model_1 = require('./src/co-browser-storage/services/co-browser-storage-model');
+exports.CoBrowserStorageModel = co_browser_storage_model_1.CoBrowserStorageModel;
+var co_browser_storage_reducer_1 = require('./src/co-browser-storage/services/co-browser-storage-reducer');
+exports.coBrowserStorageReducer = co_browser_storage_reducer_1.coBrowserStorageReducer;
+var co_browser_storage_cmp_1 = require('./src/co-browser-storage/co-browser-storage-cmp');
+exports.CoBrowserStorageCmp = co_browser_storage_cmp_1.CoBrowserStorageCmp;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY28tYnJvd3Nlci1zdG9yYWdlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY28tYnJvd3Nlci1zdG9yYWdlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSx5Q0FBb0MsNERBQ3BDLENBQUM7QUFETyxpRkFBd0Y7QUFDaEcsMkNBQXNDLDhEQUN0QyxDQUFDO0FBRE8sdUZBQTRGO0FBQ3BHLHVDQUFrQyxpREFBaUQsQ0FBQztBQUE1RSwyRUFBMkUifQ==
---
+++ co-browser-storage.ts
@@ -0,0 +1,3 @@
+export {CoBrowserStorageModel} from './src/co-browser-storage/services/co-browser-storage-model'
+export {coBrowserStorageReducer} from './src/co-browser-storage/services/co-browser-storage-reducer'
+export {CoBrowserStorageCmp} from './src/co-browser-storage/co-browser-storage-cmp'
