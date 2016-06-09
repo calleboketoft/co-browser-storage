e00cf8308c13f971ac1c43fb5a2cf56acdbde274
Carl-Ola Boketoft <calleboketoft@gmail.com>
2016-06-09 08:28:02 +0200

    simplify example

--- src/example/app-cmp.js
+++ src/example/app-cmp.js
@@ -11,24 +11,23 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var cbs_cmp_1 = require('../co-browser-storage/cbs-cmp');
 var cbs_model_1 = require('../co-browser-storage/services/cbs-model');
-require('rxjs/add/operator/find');
-require('rxjs/add/operator/map');
+var example_db_config_1 = require('./example-db-config');
 var AppCmp = (function () {
     function AppCmp(cbsModel) {
         this.cbsModel = cbsModel;
-        this.debugMode$ = this.cbsModel.getItemByKey('debugMode');
-        this.debugModeTrue$ = this.cbsModel.truthy('debugMode');
-        this.debugAndOffline$ = this.cbsModel.truthy(['debugMode', 'offlineMode']);
+        this.debugMode$ = this.cbsModel.getItemByKey(example_db_config_1.DEBUG_MODE);
+        this.debugModeTrue$ = this.cbsModel.truthy(example_db_config_1.DEBUG_MODE);
+        this.debugAndOffline$ = this.cbsModel.truthy([example_db_config_1.DEBUG_MODE, example_db_config_1.OFFLINE_MODE]);
     }
     AppCmp = __decorate([
         core_1.Component({
             selector: 'app-cmp',
             directives: [cbs_cmp_1.CbsCmp],
-            template: "\n    <h2>co-browser-storage example app</h2>\n    <br>\n\n    <div class='row'>\n      <div class='col-xs-12'>\n        <cbs-cmp></cbs-cmp>\n      </div>\n    </div>\n    <strong>debugMode value:</strong>\n    {{(debugMode$ | async).value}}\n    <br>\n    <strong>debugMode && offlineMode truthy:</strong>\n    {{debugAndOffline$ | async}}\n    <br>\n    <strong>debugMode truthy</strong>\n    {{debugModeTrue$ | async}}\n  "
+            template: "\n    <h2>co-browser-storage example app</h2>\n    <br>\n\n    <cbs-cmp></cbs-cmp>\n\n    <p>\n      <strong>debugMode value:</strong>\n      {{(debugMode$ | async).value}}\n    </p>\n\n    <p>\n      <strong>debugMode && offlineMode truthy:</strong>\n      {{debugAndOffline$ | async}}\n    </p>\n\n    <p>\n      <strong>debugMode truthy</strong>\n      {{debugModeTrue$ | async}}\n    </p>\n  "
         }), 
         __metadata('design:paramtypes', [cbs_model_1.CbsModel])
     ], AppCmp);
     return AppCmp;
 }());
 exports.AppCmp = AppCmp;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLWNtcC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImFwcC1jbXAudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLHFCQUF3QixlQUN4QixDQUFDLENBRHNDO0FBQ3ZDLHdCQUFxQiwrQkFDckIsQ0FBQyxDQURtRDtBQUNwRCwwQkFBdUIsMENBQ3ZCLENBQUMsQ0FEZ0U7QUFDakUsUUFBTyx3QkFDUCxDQUFDLENBRDhCO0FBQy9CLFFBQU8sdUJBRVAsQ0FBQyxDQUY2QjtBQXdCOUI7SUFLRSxnQkFBcUIsUUFBa0I7UUFBbEIsYUFBUSxHQUFSLFFBQVEsQ0FBVTtRQUpoQyxlQUFVLEdBQUcsSUFBSSxDQUFDLFFBQVEsQ0FBQyxZQUFZLENBQUMsV0FBVyxDQUFDLENBQUM7UUFDckQsbUJBQWMsR0FBRyxJQUFJLENBQUMsUUFBUSxDQUFDLE1BQU0sQ0FBQyxXQUFXLENBQUMsQ0FBQztRQUNuRCxxQkFBZ0IsR0FBRyxJQUFJLENBQUMsUUFBUSxDQUFDLE1BQU0sQ0FBQyxDQUFDLFdBQVcsRUFBRSxhQUFhLENBQUMsQ0FBQyxDQUFDO0lBRW5DLENBQUM7SUEzQjdDO1FBQUMsZ0JBQVMsQ0FBQztZQUNULFFBQVEsRUFBRSxTQUFTO1lBQ25CLFVBQVUsRUFBRSxDQUFDLGdCQUFNLENBQUM7WUFDcEIsUUFBUSxFQUFFLDJhQWlCVDtTQUNGLENBQUM7O2NBQUE7SUFPRixhQUFDO0FBQUQsQ0FBQyxBQU5ELElBTUM7QUFOWSxjQUFNLFNBTWxCLENBQUEifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLWNtcC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImFwcC1jbXAudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLHFCQUF3QixlQUN4QixDQUFDLENBRHNDO0FBQ3ZDLHdCQUFxQiwrQkFDckIsQ0FBQyxDQURtRDtBQUNwRCwwQkFBdUIsMENBQ3ZCLENBQUMsQ0FEZ0U7QUFDakUsa0NBSU8scUJBRVAsQ0FBQyxDQUYyQjtBQTJCNUI7SUFLRSxnQkFBcUIsUUFBa0I7UUFBbEIsYUFBUSxHQUFSLFFBQVEsQ0FBVTtRQUpoQyxlQUFVLEdBQUcsSUFBSSxDQUFDLFFBQVEsQ0FBQyxZQUFZLENBQUMsOEJBQVUsQ0FBQyxDQUFDO1FBQ3BELG1CQUFjLEdBQUcsSUFBSSxDQUFDLFFBQVEsQ0FBQyxNQUFNLENBQUMsOEJBQVUsQ0FBQyxDQUFDO1FBQ2xELHFCQUFnQixHQUFHLElBQUksQ0FBQyxRQUFRLENBQUMsTUFBTSxDQUFDLENBQUMsOEJBQVUsRUFBRSxnQ0FBWSxDQUFDLENBQUMsQ0FBQztJQUVqQyxDQUFDO0lBOUI3QztRQUFDLGdCQUFTLENBQUM7WUFDVCxRQUFRLEVBQUUsU0FBUztZQUNuQixVQUFVLEVBQUUsQ0FBQyxnQkFBTSxDQUFDO1lBQ3BCLFFBQVEsRUFBRSw4WUFvQlQ7U0FDRixDQUFDOztjQUFBO0lBT0YsYUFBQztBQUFELENBQUMsQUFORCxJQU1DO0FBTlksY0FBTSxTQU1sQixDQUFBIn0=
--- src/example/app-cmp.ts
+++ src/example/app-cmp.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core'
 import {CbsCmp} from '../co-browser-storage/cbs-cmp'
 import {CbsModel} from '../co-browser-storage/services/cbs-model'
-import 'rxjs/add/operator/find'
-import 'rxjs/add/operator/map'
+import {
+  DEBUG_MODE,
+  OFFLINE_MODE,
+  MY_PASS
+} from './example-db-config'
 
 @Component({
   selector: 'app-cmp',
@@ -11,25 +14,28 @@ import 'rxjs/add/operator/map'
     <h2>co-browser-storage example app</h2>
     <br>
 
-    <div class='row'>
-      <div class='col-xs-12'>
-        <cbs-cmp></cbs-cmp>
-      </div>
-    </div>
-    <strong>debugMode value:</strong>
-    {{(debugMode$ | async).value}}
-    <br>
-    <strong>debugMode && offlineMode truthy:</strong>
-    {{debugAndOffline$ | async}}
-    <br>
-    <strong>debugMode truthy</strong>
-    {{debugModeTrue$ | async}}
+    <cbs-cmp></cbs-cmp>
+
+    <p>
+      <strong>debugMode value:</strong>
+      {{(debugMode$ | async).value}}
+    </p>
+
+    <p>
+      <strong>debugMode && offlineMode truthy:</strong>
+      {{debugAndOffline$ | async}}
+    </p>
+
+    <p>
+      <strong>debugMode truthy</strong>
+      {{debugModeTrue$ | async}}
+    </p>
   `
 })
 export class AppCmp {
-  public debugMode$ = this.cbsModel.getItemByKey('debugMode');
-  public debugModeTrue$ = this.cbsModel.truthy('debugMode');
-  public debugAndOffline$ = this.cbsModel.truthy(['debugMode', 'offlineMode']);
+  public debugMode$ = this.cbsModel.getItemByKey(DEBUG_MODE);
+  public debugModeTrue$ = this.cbsModel.truthy(DEBUG_MODE);
+  public debugAndOffline$ = this.cbsModel.truthy([DEBUG_MODE, OFFLINE_MODE]);
 
   constructor (private cbsModel: CbsModel) {}
 }
