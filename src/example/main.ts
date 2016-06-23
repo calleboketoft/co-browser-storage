1ba93e731d0c0b2722bfe2db1256043d73431053
Carl-Ola Boketoft <calleboketoft@gmail.com>
2016-06-23 16:01:48 +0200

    better import

--- src/example/main.ts
+++ src/example/main.ts
@@ -1,10 +1,10 @@
 import {bootstrap} from '@angular/platform-browser-dynamic'
 import {provideStore} from '@ngrx/store'
 import {AppComponent} from './app.component'
-import {cbsReducer} from '../co-browser-storage/services/cbs.reducer'
 import {exampleDbConfig} from './example-db.config'
 import {provideForms} from '@angular/forms'
 import {
+  cbsReducer,
   CbsModel,
   initializeCbs,
   getInitialCbsState
